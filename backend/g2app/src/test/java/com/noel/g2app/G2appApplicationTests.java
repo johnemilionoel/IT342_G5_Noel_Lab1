3da31c0e4b52fd2db6d3913ea4504a1b975d1f78
LAB24_USER ON WS36 <L24X07W36@LabHCI.net>
2026-02-09 08:28:34 +0800

    Add Spring Boot backend project

---
+++ backend/g2app/src/test/java/com/noel/g2app/G2appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noel.g2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
